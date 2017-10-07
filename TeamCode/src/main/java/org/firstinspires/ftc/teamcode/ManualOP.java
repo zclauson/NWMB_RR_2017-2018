b36d06f6145703cd192d44c5fd7ee9148e4c8e8b
zclauson <zacrocks011@gmail.com>
2017-10-07 12:06:10 -0500

    first commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualOP.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by Zachary Clauson on 10/7/2017.
+ */
+
+public class ManualOP extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
